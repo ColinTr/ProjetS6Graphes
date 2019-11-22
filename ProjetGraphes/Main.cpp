6778e7df04c31f62e9e474b83b32d3db78babf7b
ColinTr <56585263+ColinTr@users.noreply.github.com>
2019-11-22 16:52:43 +0100

    Ajoutez des fichiers projet.

---
+++ ProjetGraphes/Main.cpp
@@ -0,0 +1,53 @@
+/**
+ * \file main.cpp
+ * \brief Fichier main de la librairie Les Graphes
+ * \author Guillaume.Bouchard Colin.Troisemaine
+ * \version 0.1
+ * \date 16 mai 2019
+ *
+ * Ce fichier comporte la fonction main.
+ *
+ */
+
+#include <iostream>
+#include "CGraphe.h"
+#include "CParser.h"
+
+int main(int argc, char *argv[])
+{
+	unsigned int uiIndiceSommet;
+    int iIndiceArc; // Non unsigned car on a besoin de faire uiIndiceArc-- lorsqu'il vaut 0.
+    CGraphe graphe;
+    CGraphe grapheInverse;
+    
+    if (argc > 1) {
+        try {
+            graphe = CGraphe(argv[1]);
+			std::cout << "========= Affichage du graphe lu dans " << argv[1] << " =========\n\n" << std::endl;
+			graphe.GRAAfficherGraphe();
+        }
+        catch (CException EXCLevee) {
+            printf("Code d'erreur levee = %d\n", EXCLevee.EXCLireErreur());
+            return 1;
+        }
+        
+		std::cout << "========= Inversion de tous les arcs du graphe =========\n\n" << std::endl;
+        
+        //On recopie d'abord tous les sommet du premier graphe :
+        for (uiIndiceSommet = 0; uiIndiceSommet < graphe.GRAGetNbSommets(); uiIndiceSommet++) {
+            grapheInverse.GRAAjouterSommet(graphe.GRAGetSommet(uiIndiceSommet)->SOMGetNumeroSommet());
+        }
+        
+        //Puis on ajoute tous les arcs en les inversants au nouveau graphe :
+        //On parcourt uniquement les arcs partants avec notre fonction GRAModifierArc qui inversera Žgalement les arcs arrivants
+        for (uiIndiceSommet = 0; uiIndiceSommet < graphe.GRAGetNbSommets(); uiIndiceSommet++) {
+            for (iIndiceArc = graphe.GRAGetSommet(uiIndiceSommet)->SOMGetNbArcsPartants() - 1; iIndiceArc >= 0 ; iIndiceArc--) {
+                grapheInverse.GRAAjouterArc(graphe.GRAGetSommet(uiIndiceSommet)->SOMGetNumSommetArcPartant(iIndiceArc), graphe.GRAGetSommet(uiIndiceSommet)->SOMGetNumeroSommet());
+            }
+        }
+        grapheInverse.GRAAfficherGraphe();
+    }
+    else {
+        std::cout << "Aucun nom de fichier passe en parametre." << std::endl;
+    }
+}
